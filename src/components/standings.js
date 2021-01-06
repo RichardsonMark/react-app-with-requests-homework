72b533327327ad737e9dc2231b0c7dbcb0aa7126
RichardsonMark <47854347+RichardsonMark@users.noreply.github.com>
2021-01-06 11:17:07 +0000

    Update standings.js

--- src/components/standings.js
+++ src/components/standings.js
@@ -8,7 +8,7 @@ const LeagueStandings = () => {
 
   const getStandings = () => {
     fetch(`https://api.football-data.org/v2/competitions/SA/standings`, {
-      headers: { "X-Auth-Token": "0c37514e7081466d8daac3d5661dffc7" },
+      headers: { "X-Auth-Token": "api_key" },
     })
     .then((res) => {
         return res.json()
